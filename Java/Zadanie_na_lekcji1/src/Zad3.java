3683ace7b84e432f4d953aa2c351e4d5f12ea14b
Naaatalia <56105871+Naaatalia@users.noreply.github.com>
2019-10-24 10:15:03 +0200

    Update Zad3.java

--- Java/Zadanie_na_lekcji1/src/Zad3.java
+++ Java/Zadanie_na_lekcji1/src/Zad3.java
@@ -2,8 +2,9 @@ import java.util.Scanner;
 public class Zad3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method 
+Scanner s = new Scanner(System.in);
+System.out.println("");
+	}// void main
 
-	}
-
-}
+}// public class
